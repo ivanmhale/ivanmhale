f82382be7fabf28150e2d714ef9d82d97ed39a5b
Ivan Hale <ivan.hale@cognizant.com>
2020-05-05 16:17:37 -0400

    it works

---
+++ src/app/app.js
@@ -0,0 +1,22 @@
+import '../styles/index.scss';
+const name = document.getElementById('name');
+const liAbout = document.getElementById('nav_about');
+const liSamples = document.getElementById('nav_samples');
+const liContact = document.getElementById('nav_contact');
+const mainContent = document.getElementById('window');
+
+name.addEventListener('click', () => {
+  mainContent.style.transform = 'translateY(0%)';
+});
+
+liAbout.addEventListener('click', () => {
+  mainContent.style.transform = 'translateY(-100%)';
+});
+
+liSamples.addEventListener('click', () => {
+  mainContent.style.transform = 'translateY(-200%)';
+});
+
+liContact.addEventListener('click', () => {
+  mainContent.style.transform = 'translateY(-300%)';
+});
